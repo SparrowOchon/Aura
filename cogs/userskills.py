d40b9c4bf866d3901c585fc9b09137606a860958
Kevin <promkev@gmail.com>
2019-07-18 00:02:06 -0400

    Upgrade command

---
+++ cogs/userskills.py
@@ -0,0 +1,34 @@
+import discord
+from discord import Member
+from discord.ext.commands import has_permissions, MissingPermissions
+from discord.ext import commands
+import typing
+
+
+class UserSkills(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def upgrade(self, ctx, upgrade: typing.Optional[str] = '', amount: typing.Optional[str] = '1'):
+        member = ctx.author
+        await self.client.pool.execute('''INSERT INTO user_skills(user_id, multi_hit_chance, multi_hit_factor, critical_chance, critical_power, status_chance, status_length) VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING'''% (int(member.id), 0, 0, 0, 0, 0, 0))
+        mc_level = await self.client.pool.fetchval('''SELECT multi_hit_chance FROM user_skills WHERE user_id = %s''' % member.id)
+        mf_level = await self.client.pool.fetchval('''SELECT multi_hit_factor FROM user_skills WHERE user_id = %s''' % member.id)
+        cc_level = await self.client.pool.fetchval('''SELECT critical_chance FROM user_skills WHERE user_id = %s''' % member.id)
+        cp_level = await self.client.pool.fetchval('''SELECT critical_power FROM user_skills WHERE user_id = %s''' % member.id)
+        sc_level = await self.client.pool.fetchval('''SELECT status_chance FROM user_skills WHERE user_id = %s''' % member.id)
+        sl_level = await self.client.pool.fetchval('''SELECT status_length FROM user_skills WHERE user_id = %s''' % member.id)
+        if upgrade == 'help' or upgrade == '':
+            embed = discord.Embed(title='Upgrades', description='Upgrade your skills to earn more points')
+            embed.add_field(name=f'[MC] Multi-hit Chance ({mc_level})', value='Chance to proc multiple messages that will also apply other upgrades', inline=False)
+            embed.add_field(name=f'[MF] Multi-hit Factor ({mf_level})', value='Number of multi-hits on proc', inline=False)
+            embed.add_field(name=f'[CC] Critical Chance ({cc_level})', value='Chance to get critical hits', inline=False)
+            embed.add_field(name=f'[CP] Critical Power ({cp_level})', value='Factor the points are multiplied by', inline=False)
+            embed.add_field(name=f'[SC] Status Chance ({sc_level})', value='Chance to apply a status effect, caps to certain percentage', inline=False)
+            embed.add_field(name=f'[SL] Status Length ({sl_level})', value='Length of the status effect', inline=False)
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(UserSkills(client))
--- main.py
+++ main.py
@@ -4,24 +4,99 @@ from discord.ext import commands
 import asyncio
 import asyncpg
 import logging
+import datetime
 
-
-# Connects to database
-async def create_db_pool():
-    pool = await asyncpg.create_pool(host='127.0.0.1', database='GameBot', user='postgres', password='admin123')
-    con = await pool.acquire()
 
 # Logs to console
 logging.basicConfig(level=logging.INFO)
 
-client = discord.Client()
 token = 'MjcyMjYwMDYyNzkyMTIyMzY4.XS9O1Q.fl1ghrEgvrLP0svKo-k5wX3lmd0'
+client = commands.Bot(command_prefix='.')
+
+# Loop to look through cogs folder and load all cogs contained within it
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        # splicing cuts 3 last characters aka .py
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+# Connects to database
+async def create_db_pool():
+    client.pool = await asyncpg.create_pool(host='127.0.0.1', database='GameBot', user='postgres', password='admin123')
+    await client.pool.execute('''CREATE TABLE IF NOT EXISTS users( 
+                        user_id BIGINT,
+                        text_messages INTEGER,
+                        voice_time INTEGER,
+                        points DECIMAL,
+                        voice_join_timestamp VARCHAR[30],
+                        PRIMARY KEY(user_id))''')
+    await client.pool.execute('''CREATE TABLE IF NOT EXISTS user_skills( 
+                        user_id BIGINT,
+                        multi_hit_chance BIGINT,
+                        multi_hit_factor BIGINT,
+                        critical_chance BIGINT,
+                        critical_power BIGINT,
+                        status_chance BIGINT,
+                        status_length BIGINT,
+                        PRIMARY KEY(user_id))''')
+    await client.pool.execute('''CREATE TABLE IF NOT EXISTS guilds( 
+                            guild_id BIGINT,
+                            PRIMARY KEY(guild_id))''')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    member = message.author
+    await client.pool.execute('''INSERT INTO users(user_id, text_messages, voice_time, points, voice_join_timestamp) VALUES(%s, %s, %s, %s, NULL) ON CONFLICT DO NOTHING''' % (int(member.id), int(0), int(0), int(0)))
+    await client.pool.execute('''INSERT INTO users(user_id, text_messages, voice_time, points, voice_join_timestamp) VALUES(%s, %s, %s, %s, NULL) ON CONFLICT DO NOTHING''' % (int(member.id), int(0), int(0), int(0)))
+    await client.pool.execute('''UPDATE users SET text_messages = text_messages+1 WHERE user_id = %s ''' % (int(member.id)))
+    print(f'{message.author} in {message.author.guild.name}: {message.content}')
+    await client.process_commands(message)
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+
+    # User joined the channel
+    if before.channel is None and after.channel is not None:
+        member_id = member.id
+        print(member, 'joined ', after.channel, ' in ', member.guild.name)
+
+        # Adds user to the database if they are not in it
+        await client.pool.execute(
+            '''INSERT INTO users(user_id, text_messages, voice_time, points) VALUES(%s, %s, %s, %s) ON CONFLICT DO NOTHING'''
+            % (int(member.id), int(0), int(0), int(0)))
+
+        # Store the current timestamp in the database
+        await client.pool.execute('''UPDATE users SET voice_join_timestamp = %s WHERE user_id = %s''', (datetime.datetime.now(), member.id))
+
+    # User left the channel
+    if before.channel is not None and after.channel is None:
+        print(member, 'left ', before.channel, ' in ', member.guild.name)
+        if member.guild.afk_channel is None or before.channel.id is not member.guild.afk_channel.id:
+            # Get the time in voice chat
+            before_timestamp = await client.pool.fetchval('''SELECT voice_join_timestamp FROM users WHERE user_id = %s''' % (member.id))
+            td = datetime.datetime.now() - before_timestamp[0]
+            td_seconds = int(td.total_seconds())
+            await client.pool.execute('''UPDATE users SET voice_time = voice_time + %s WHERE user_id = %s''' % (td_seconds, member.id))
+            # Add points
+        # Wipe timestamp
+        await client.pool.execute('''UPDATE users SET voice_join_timestamp = NULL WHERE user_id = %s''' % (member.id))
 
 
 @client.event
 async def on_ready():
     print('Bot is ready')
+    guild_list = []
+    for guild in list(client.guilds):
+        guild_list.append(guild.name)
+        await client.pool.execute('''INSERT INTO guilds(guild_id) VALUES(%s) ON CONFLICT DO NOTHING'''% (int(guild.id)))
+    # Wipes timestamps
+    await client.pool.execute('''ALTER TABLE users DROP COLUMN voice_join_timestamp CASCADE''')
+    await client.pool.execute('''ALTER TABLE users ADD COLUMN IF NOT EXISTS voice_join_timestamp TIMESTAMP''')
 
 # Athena token
-client.run(token)
 asyncio.get_event_loop().run_until_complete(create_db_pool())
+client.run(token)
