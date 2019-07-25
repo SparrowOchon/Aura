d135ec07cc61da619fe7e95b84ca2c0b1483dd84
Kevin <promkev@gmail.com>
2019-07-25 14:43:58 -0400

    Edited help command

--- cogs/help.py
+++ cogs/help.py
@@ -12,6 +12,7 @@ class Help(commands.Cog):
         embed.add_field(name=f'Profile', value=f'Show your profile or someone else\'s. \nCommand: __{ctx.prefix}profile__ or __{ctx.prefix}p__')
         embed.add_field(name=f'Upgrade', value=f'Upgrade your skills. \nCommand: __{ctx.prefix}upgrade__ or __{ctx.prefix}up__')
         embed.add_field(name=f'Rebirth', value=f'Rebirth resets your stats but boosts your silver gain. \nCommand: __{ctx.prefix}rebirth__')
+        embed.add_field(name=f'Leaderboards', value=f'Check the global leaderboards \nCommand: __{ctx.prefix}leaderboards__')
         await ctx.send(embed=embed)
 
 
