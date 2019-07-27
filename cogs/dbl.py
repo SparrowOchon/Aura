6c584f5d0c9b13c1fe439a151c5414add3a068f4
Kevin <promkev@gmail.com>
2019-07-27 14:27:35 -0400

    on_guild_join adds new guilds in the database.

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -25,6 +25,7 @@ class DiscordBotsOrgAPI(commands.Cog):
             await asyncio.sleep(1800)
 
 
+
 def setup(client):
     global logger
     logger = logging.getLogger('client')
--- cogs/guilds.py
+++ cogs/guilds.py
@@ -8,6 +8,11 @@ class Guilds(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    async def on_guild_join(self, guild):
+        channel = self.client.get_channel(604741076900642826)
+        await self.client.pool.execute('''INSERT INTO guilds(guild_id, prefix, count) VALUES(%s, $$%s$$, %s) ON CONFLICT DO NOTHING''' % (int(guild.id), '.', int(guild.member_count)))
+        await channel.sent(f'{guild.name} added {self.client.user.name}')
+
 
 def setup(client):
     client.add_cog(Guilds(client))
