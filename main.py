cd2cfce2dfc7dadc5683f74dca5e91d87e9e1e19
Kevin <promkev@gmail.com>
2019-07-26 23:55:07 -0400

    Disabled points on voice exit due to infinite points bug

--- main.py
+++ main.py
@@ -226,116 +226,116 @@ async def on_voice_state_update(member, before, after):
     # User left the channel
     if before.channel is not None and after.channel is None:
         print(member, 'left ', before.channel, ' in ', member.guild.name)
-        if member.guild.afk_channel is None or before.channel.id is not member.guild.afk_channel.id:
-
-            # Get the time in voice chat
-            before_timestamp = await client.pool.fetchval('''SELECT voice_join_timestamp FROM users WHERE user_id = %s''' % (member.id))
-            now = datetime.datetime.now()
-            if before_timestamp is None:
-                await client.pool.execute(
-                '''UPDATE users SET voice_join_timestamp = $$%s$$ WHERE user_id = %s''' % (
-                    now, member.id))
-                before_timestamp = now
-            td = now - before_timestamp
-            td_seconds = int(td.total_seconds())
-            print(td_seconds)
-            await client.pool.execute('''UPDATE users SET voice_time = voice_time + %s WHERE user_id = %s''' % (td_seconds, member.id))
-            hits = round(td_seconds/30)
-
-            # Adds user into the user_skill database if they are not there
-            await client.pool.execute(
-                '''INSERT INTO user_skills(user_id, multi_hit_chance, multi_hit_factor, critical_chance, critical_power, status_chance, status_length) VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING''' % (
-                    int(member.id), 0, 0, 0, 0, 0, 0))
-
-            # Retrieves levels
-            mc_level = await client.pool.fetchval(
-                '''SELECT multi_hit_chance FROM user_skills WHERE user_id = %s''' % member.id)
-            mc = ['mc', 'multi-hit chance']
-            mf_level = await client.pool.fetchval(
-                '''SELECT multi_hit_factor FROM user_skills WHERE user_id = %s''' % member.id)
-            mf = ['mf', 'multi-hit factor']
-            cc_level = await client.pool.fetchval(
-                '''SELECT critical_chance FROM user_skills WHERE user_id = %s''' % member.id)
-            cc = ['cc', 'critical chance']
-            cp_level = await client.pool.fetchval(
-                '''SELECT critical_power FROM user_skills WHERE user_id = %s''' % member.id)
-            cp = ['cp', 'critical power']
-            sc_level = await client.pool.fetchval(
-                '''SELECT status_chance FROM user_skills WHERE user_id = %s''' % member.id)
-            sc = ['sc', 'status chance']
-            sl_level = await client.pool.fetchval(
-                '''SELECT status_length FROM user_skills WHERE user_id = %s''' % member.id)
-            sl = ['sc', 'status length']
-
-            # Points per message
-            ppm = 1
-            points = 0
-
-            while True:
-                if hits == 0:
-                    break
-                hits = hits-1
-
-                # Calculates multi-hit
-                multi_chance = mc_level * 0.5
-                multi_factor = 2 + (mf_level)
-                multi_hits = 0
-                if multi_chance > 100:
-                    # print('Multi-hit chance above 100!')
-                    while True:
-                        multi_chance = multi_chance - 100
-                        multi_hits = multi_hits + 1
-                        if multi_chance < 100:
-                            break
-                if random.randint(1, 100) < multi_chance:
-                    multi_hits = multi_hits + 1
-
-                multi_msg = multi_hits * multi_factor
-                # print(f'Multi hits {multi_hits}')
-                # print(f'Multi factor {multi_factor}')
-                # print(f'Multi_msg {multi_msg}')
-                msg = multi_msg + 1
-                # print(f'Message count: {msg}')
-                # Calculates critical hits
-                critical_chance = cc_level * 1
-                critical_hits = 0
-                critical_power = cp_level + 2
-                if critical_chance > 100:
-                    while True:
-                        critical_chance = critical_chance - 100
-                        critical_hits = critical_hits + 1
-                        if critical_chance < 100:
-                            break
-                # print(f'CC {critical_chance}%')
-                while True:
-                    msg = msg - 1
-                    if random.randint(1, 100) < critical_chance:
-                        # print('Critical proc!')
-                        points = points + critical_power * ppm
-                    else:
-                        points = points + ppm
-                    if msg == 0:
-                        break
-                # Calculating points
-                # print(f'CH {critical_hits}')
-                if int(critical_hits) > 0:
-                    # print('Critical hits detected')
-                    points = points * critical_hits * critical_power * ppm
-                if hits == 0:
-                    break
-
-                lifetime_flowers = await client.pool.fetchval(
-                    '''SELECT lifetimeflowers FROM users WHERE user_id =%d''' % (int(member.id),))
-                flowers_boost = 1 + float(lifetime_flowers) * 0.01
-                points = points * flowers_boost
-
-                print(f'Points {points:,}')
-                # Adds points to the database
-                await client.pool.execute(
-                    '''UPDATE users SET points = points+%s WHERE user_id = %s ''' % (points, int(member.id)))
-                # Adds lifetime points to the database
-                await client.pool.execute(
-                    '''UPDATE users SET lifetime = lifetime+%s WHERE user_id = %s ''' % (points, int(member.id)))
+        # if member.guild.afk_channel is None or before.channel.id is not member.guild.afk_channel.id:
+        #
+        #     # Get the time in voice chat
+        #     before_timestamp = await client.pool.fetchval('''SELECT voice_join_timestamp FROM users WHERE user_id = %s''' % (member.id))
+        #     now = datetime.datetime.now()
+        #     if before_timestamp is None:
+        #         await client.pool.execute(
+        #         '''UPDATE users SET voice_join_timestamp = $$%s$$ WHERE user_id = %s''' % (
+        #             now, member.id))
+        #         before_timestamp = now
+        #     td = now - before_timestamp
+        #     td_seconds = int(td.total_seconds())
+        #     print(td_seconds)
+        #     await client.pool.execute('''UPDATE users SET voice_time = voice_time + %s WHERE user_id = %s''' % (td_seconds, member.id))
+        #     hits = round(td_seconds/30)
+        #
+        #     # Adds user into the user_skill database if they are not there
+        #     await client.pool.execute(
+        #         '''INSERT INTO user_skills(user_id, multi_hit_chance, multi_hit_factor, critical_chance, critical_power, status_chance, status_length) VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING''' % (
+        #             int(member.id), 0, 0, 0, 0, 0, 0))
+        #
+        #     # Retrieves levels
+        #     mc_level = await client.pool.fetchval(
+        #         '''SELECT multi_hit_chance FROM user_skills WHERE user_id = %s''' % member.id)
+        #     mc = ['mc', 'multi-hit chance']
+        #     mf_level = await client.pool.fetchval(
+        #         '''SELECT multi_hit_factor FROM user_skills WHERE user_id = %s''' % member.id)
+        #     mf = ['mf', 'multi-hit factor']
+        #     cc_level = await client.pool.fetchval(
+        #         '''SELECT critical_chance FROM user_skills WHERE user_id = %s''' % member.id)
+        #     cc = ['cc', 'critical chance']
+        #     cp_level = await client.pool.fetchval(
+        #         '''SELECT critical_power FROM user_skills WHERE user_id = %s''' % member.id)
+        #     cp = ['cp', 'critical power']
+        #     sc_level = await client.pool.fetchval(
+        #         '''SELECT status_chance FROM user_skills WHERE user_id = %s''' % member.id)
+        #     sc = ['sc', 'status chance']
+        #     sl_level = await client.pool.fetchval(
+        #         '''SELECT status_length FROM user_skills WHERE user_id = %s''' % member.id)
+        #     sl = ['sc', 'status length']
+        #
+        #     # Points per message
+        #     ppm = 1
+        #     points = 0
+        #
+        #     while True:
+        #         if hits == 0:
+        #             break
+        #         hits = hits-1
+        #
+        #         # Calculates multi-hit
+        #         multi_chance = mc_level * 0.5
+        #         multi_factor = 2 + (mf_level)
+        #         multi_hits = 0
+        #         if multi_chance > 100:
+        #             # print('Multi-hit chance above 100!')
+        #             while True:
+        #                 multi_chance = multi_chance - 100
+        #                 multi_hits = multi_hits + 1
+        #                 if multi_chance < 100:
+        #                     break
+        #         if random.randint(1, 100) < multi_chance:
+        #             multi_hits = multi_hits + 1
+        #
+        #         multi_msg = multi_hits * multi_factor
+        #         # print(f'Multi hits {multi_hits}')
+        #         # print(f'Multi factor {multi_factor}')
+        #         # print(f'Multi_msg {multi_msg}')
+        #         msg = multi_msg + 1
+        #         # print(f'Message count: {msg}')
+        #         # Calculates critical hits
+        #         critical_chance = cc_level * 1
+        #         critical_hits = 0
+        #         critical_power = cp_level + 2
+        #         if critical_chance > 100:
+        #             while True:
+        #                 critical_chance = critical_chance - 100
+        #                 critical_hits = critical_hits + 1
+        #                 if critical_chance < 100:
+        #                     break
+        #         # print(f'CC {critical_chance}%')
+        #         while True:
+        #             msg = msg - 1
+        #             if random.randint(1, 100) < critical_chance:
+        #                 # print('Critical proc!')
+        #                 points = points + critical_power * ppm
+        #             else:
+        #                 points = points + ppm
+        #             if msg == 0:
+        #                 break
+        #         # Calculating points
+        #         # print(f'CH {critical_hits}')
+        #         if int(critical_hits) > 0:
+        #             # print('Critical hits detected')
+        #             points = points * critical_hits * critical_power * ppm
+        #         if hits == 0:
+        #             break
+        #
+        #         lifetime_flowers = await client.pool.fetchval(
+        #             '''SELECT lifetimeflowers FROM users WHERE user_id =%d''' % (int(member.id),))
+        #         flowers_boost = 1 + float(lifetime_flowers) * 0.01
+        #         points = points * flowers_boost
+        #
+        #         print(f'Points {points:,}')
+        #         # Adds points to the database
+        #         await client.pool.execute(
+        #             '''UPDATE users SET points = points+%s WHERE user_id = %s ''' % (points, int(member.id)))
+        #         # Adds lifetime points to the database
+        #         await client.pool.execute(
+        #             '''UPDATE users SET lifetime = lifetime+%s WHERE user_id = %s ''' % (points, int(member.id)))
 
         # Wipes user's timestamp
         await client.pool.execute('''UPDATE users SET voice_join_timestamp = NULL WHERE user_id = %s''' % (member.id))
